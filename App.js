1f161db383555b5613048deca2ed20f249bf2456
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-23 21:00:13 +0500

    Import Login Component in App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Login from './src/screens/auth/login'
+
+export default function App() {
+  return(
+    // <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    //   <Text>App.js</Text>
+    // </View>
+    <Login/>
+  )
+}
