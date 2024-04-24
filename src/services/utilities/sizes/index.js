f5adce73b1bc56dedd7908474f7147a7dbbcb3a6
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-24 18:11:53 +0500

    Creating JavaScript object named fontSizes

---
+++ src/services/utilities/sizes/index.js
@@ -0,0 +1,18 @@
+import { totalSize } from 'react-native-dimension'
+
+export const fontSizes = {
+    // Headings
+    h1: totalSize(4),
+    h2: totalSize(3.6),
+    h3: totalSize(3.2),
+    h4: totalSize(2.8),
+    h5: totalSize(2.4),
+    h6: totalSize(2),
+    // Normal Text
+    large: totalSize(1.8),
+    medium: totalSize(1.6),
+    regular: totalSize(1.4),
+    small: totalSize(1.2),
+    tiny: totalSize(1),
+    xTiny: totalSize(0.8),
+}
