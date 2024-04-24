d093d8d53beb1132cafb2eb4ebf0406314898932
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-24 17:55:33 +0500

    Defining JavaScript Object named fonts

---
+++ src/services/utilities/fonts/index.js
@@ -0,0 +1,14 @@
+export const fonts = {
+
+    // Primary Font
+    appFont1Regular: 'Poppins-Regular.ttf',
+    appFont1Light: 'Poppins-Light.ttf',
+    appFont1Medium: 'Poppins-Medium.ttf',
+    appFont1Bold: 'Poppins-Bold.ttf',
+    // Secondary Font
+    appFont2Regular: 'Poppins-Regular.ttf',
+    appFont2Medium: 'Poppins-Medium.ttf',
+    appFont2Light: 'Poppins-Light.ttf',
+    appFont2Bold: 'Poppins-Bold.ttf',
+    appFont2ExtraBold: 'Poppins-ExtraBold.ttf',
+}
