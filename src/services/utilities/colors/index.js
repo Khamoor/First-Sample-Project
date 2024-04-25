892dd8e63c13738f876739182a2444059e2e7d69
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-25 17:30:48 +0500

    Added some more Colors

--- src/services/utilities/colors/index.js
+++ src/services/utilities/colors/index.js
@@ -1,6 +1,10 @@
 export const colors = {
+    appGradient1: ['#FFFFFF', '#FFFFCC'],
     appColor1: '#FFFFC8',
 
     appTextColor1: '#000000',
     appTextColor2: '#444444',
+    appTextColor3: '#222222',
+    appTextColor4: '#FFFFFF',
+    appTextColor5: '#FFFFC8',
 }
