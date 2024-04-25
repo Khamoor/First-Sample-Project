bbcef579dcd7f35c9706a438f67fb7e27f479aa6
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-25 17:32:55 +0500

    Removed ".tff" from Font Names

--- src/services/utilities/fonts/index.js
+++ src/services/utilities/fonts/index.js
@@ -1,14 +1,14 @@
 export const fonts = {
 
     // Primary Font
-    appFont1Regular: 'Roboto-Regular.ttf',
-    appFont1Light: 'Roboto-Light.ttf',
-    appFont1Medium: 'Roboto-Medium.ttf',
-    appFont1Bold: 'Roboto-Bold.ttf',
+    appFont1Regular: 'Roboto-Regular',
+    appFont1Light: 'Roboto-Light',
+    appFont1Medium: 'Roboto-Medium',
+    appFont1Bold: 'Roboto-Bold',
     // Secondary Font
-    appFont2Regular: 'Poppins-Regular.ttf',
-    appFont2Medium: 'Poppins-Medium.ttf',
-    appFont2Light: 'Poppins-Light.ttf',
-    appFont2Bold: 'Poppins-Bold.ttf',
-    appFont2ExtraBold: 'Poppins-ExtraBold.ttf',
+    appFont2Regular: 'Poppins-Regular',
+    appFont2Medium: 'Poppins-Medium',
+    appFont2Light: 'Poppins-Light',
+    appFont2Bold: 'Poppins-Bold',
+    appFont2ExtraBold: 'Poppins-ExtraBold',
 }
