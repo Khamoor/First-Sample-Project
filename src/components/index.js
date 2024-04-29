a78675b6699e34e0166a80c29a0f5444209d459f
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-29 19:02:03 +0500

    Exporting 'textinputs'

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Texts from './texts'
 import Spacer from './spacer'
 import * as Scrollviews from './scrollviews'
+import * as Textinputs from './textinputs'
 
-export { Texts, Spacer, Scrollviews }
+export { Texts, Spacer, Scrollviews, Textinputs }
