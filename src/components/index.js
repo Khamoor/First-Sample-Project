52e1368d7ac76b8b82e4b29df7cd6e1293d381c6
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-29 19:56:07 +0500

    Exporting 'Buttons' Component.

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Texts from './texts'
 import Spacer from './spacer'
 import * as Scrollviews from './scrollviews'
 import * as Textinputs from './textinputs'
+import * as Buttons from './buttons'
 
-export { Texts, Spacer, Scrollviews, Textinputs }
+export { Texts, Spacer, Scrollviews, Textinputs, Buttons }
