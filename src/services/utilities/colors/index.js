f057cc14a20e93f25f0ad6774c9c6b4efa81942a
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-04-29 16:25:51 +0500

    Added one color

--- src/services/utilities/colors/index.js
+++ src/services/utilities/colors/index.js
@@ -1,10 +1,11 @@
 export const colors = {
-    appGradient1: ['#FFFFFF', '#FFFFCC'],
-    appColor1: '#FFFFC8',
+    appGradient1: ['#FFFFFF', '#FFFFCC'],   // Gradient from White to Light Yellow
+    appColor1: '#FFFFC8',   // Light Yellow
+    appColor2: '#FFFFC8',
 
-    appTextColor1: '#000000',
-    appTextColor2: '#444444',
-    appTextColor3: '#222222',
-    appTextColor4: '#FFFFFF',
-    appTextColor5: '#FFFFC8',
+    appTextColor1: '#000000',   // Black
+    appTextColor2: '#444444',   // Dark Gray
+    appTextColor3: '#222222',   // Dark Shade of Black
+    appTextColor4: '#FFFFFF',   // White
+    appTextColor5: '#FFFFC8',   // Shade of Light Yellow
 }
