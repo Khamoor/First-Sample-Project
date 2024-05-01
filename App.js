d9eaaa7dcc46d2ae77ff589fd59443978cdc9ebc
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-01 18:20:29 +0500

    Added 'Navigation' Component instead of 'Login'

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import Login from './src/screens/auth/login'
+import Navigation from './src/navigation'
 
 export default function App() {
   return(
     // <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
     //   <Text>App.js</Text>
     // </View>
-    <Login/>
+    // <Login/>
+    <Navigation/>
   )
 }
