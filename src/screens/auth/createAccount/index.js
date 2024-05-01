2b03b78026d4da395369e0f8838c87568e87d517
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-01 16:54:59 +0500

    Defining 'Index' Component

---
+++ src/screens/auth/createAccount/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Texts, Wrapper } from '../../../components'
+
+export default function Index() {
+    return (
+        <Wrapper isMain center>
+            <Texts isH4>Create Account</Texts>
+        </Wrapper>
+    )
+}
