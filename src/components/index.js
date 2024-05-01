0d677c74c26a605c58cc387822c2ee176c9be5a5
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-01 08:59:01 +0500

    Exporting 'Wrapper' & 'BgImageWrapper' as 'Common' Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Texts from './texts'
 import Spacer from './spacer'
+import Wrapper from './wrapper'
 import * as Scrollviews from './scrollviews'
 import * as Textinputs from './textinputs'
 import * as Buttons from './buttons'
+import * as Common from './common'
 
-export { Texts, Spacer, Scrollviews, Textinputs, Buttons }
+export { Texts, Spacer, Scrollviews, Textinputs, Buttons, Wrapper, Common }
