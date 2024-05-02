3e902bd71c529901fb882c00751a3e3915581ad1
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-02 15:52:43 +0500

    Export 'Headers' Component

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import Wrapper from './wrapper'
 import * as Scrollviews from './scrollviews'
 import * as Textinputs from './textinputs'
 import * as Buttons from './buttons'
+import * as Headers from './headers'
 import * as Common from './common'
 
-export { Texts, Spacer, Scrollviews, Textinputs, Buttons, Wrapper, Common }
+export { Texts, Spacer, Scrollviews, Textinputs, Buttons, Wrapper, Headers, Common }
