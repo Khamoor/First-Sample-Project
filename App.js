e2cf4a0dfcf8cd652bb0d02e45bd35193f09b475
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-02 15:37:54 +0500

    Added 'SafeAreaProvider' Component

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import Login from './src/screens/auth/login'
 import Navigation from './src/navigation'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return(
+  return (
     // <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
     //   <Text>App.js</Text>
     // </View>
     // <Login/>
-    <Navigation/>
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   )
 }
