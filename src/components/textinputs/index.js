7bdb55bf7394fd09ed62f54bfc3a274d7e56c57d
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-08 18:41:47 +0500

    Added 'containerStyle' prop in 'TextInputs' component

--- src/components/textinputs/index.js
+++ src/components/textinputs/index.js
@@ -4,13 +4,14 @@ import { colors, fontSizes, fonts } from '../../services/utilities'
 import { height, width, totalSize } from 'react-native-dimension'
 
 export function Colored({
+    containerStyle,
     title,
     value,
     onChangeText,
     ...props
 }) {
     return (
-        <View style={{ marginHorizontal: width(7.5), backgroundColor: colors.appColor1, borderRadius: 10, paddingHorizontal: width(2.5) }}>
+        <View style={[{ marginHorizontal: width(5), backgroundColor: colors.appColor1, borderRadius: 10, paddingHorizontal: width(2.5), }, containerStyle]}>
             <Text style={{ fontSize: fontSizes.tiny, fontFamily: fonts.appFont1Bold, color: colors.appTextColor2, paddingTop: height(1) }}>{title}</Text>
             <TextInput
                 value={value}
