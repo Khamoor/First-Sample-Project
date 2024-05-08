8cfeec2eb2baf99f2c4d156a10b8e91a1dd419ac
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-08 18:28:32 +0500

    Added 'flex' prop in 'Wrapper' component

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -4,12 +4,16 @@ import { appStyles } from '../../services/utilities'
 
 export default function Wrapper({
     children,
+    flex,
     isMain,
     center,
 }) {
     return (
         <View
             style={[
+                {
+                    flex: flex
+                },
                 isMain && appStyles.mainContainer,
                 center && appStyles.center
             ]}
