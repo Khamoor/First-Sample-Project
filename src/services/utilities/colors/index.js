6d74bad9827b27be9e68cf63a1c5cf74841f2b7b
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-09 18:28:17 +0500

    Added 'appBackgroundColor1' & 'appTextColor6'

--- src/services/utilities/colors/index.js
+++ src/services/utilities/colors/index.js
@@ -1,11 +1,17 @@
 export const colors = {
     appGradient1: ['#FFFFFF', '#FFFFCC'],   // Gradient from White to Light Yellow
+    // App Colors
     appColor1: '#FFFFC8',   // Light Yellow
     appColor2: '#FFFFC8',
 
+    // Text Colors
     appTextColor1: '#000000',   // Black
     appTextColor2: '#444444',   // Dark Gray
     appTextColor3: '#222222',   // Dark Shade of Black
     appTextColor4: '#FFFFFF',   // White
     appTextColor5: '#FFFFC8',   // Shade of Light Yellow
+    appTextColor6: '#444444D4',
+
+    // Background Colors
+    appBackgrounColor1: '#FFFFFF',
 }
