2dab3eac158f5cd165fbc89efd7fc7e45b62e95b
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-12 12:06:25 +0500

    Added 'user' icon in appIcons constant

--- src/services/utilities/assets/index.js
+++ src/services/utilities/assets/index.js
@@ -1,7 +1,8 @@
-export const appImages={
+export const appImages = {
     background_1: require('../../../assets/images/background_1.png'),
     logo_1: require('../../../assets/images/logo_1.png'),
 }
-export const appIcons={
+export const appIcons = {
     calendar: require('../../../assets/icons/calendar.png'),
+    user: require('../../../assets/icons/user.png'),
 }
