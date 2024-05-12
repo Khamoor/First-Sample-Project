d9747e7ce7454e338b7dbb09cdd7cf3e0d4d6b1c
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-12 14:01:43 +0500

    Adding 'isTiny' prop in 'Spacer' component

--- src/components/spacer/index.js
+++ src/components/spacer/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { View } from 'react-native'
 import { height as h, width as w } from 'react-native-dimension'
 
-export default function Spacer({ isBasic, isSmall, isLarge, height, width }) {
+export default function Spacer({ isBasic, isSmall, isTiny, isLarge, height, width }) {
 
-    const defaultHeight = isLarge ? h(5) : isBasic ? h(2.5) : isSmall ? h(1.25) : height
-    const defaultWidth = isLarge ? w(10) : isBasic ? w(5) : isSmall ? w(2.25) : width
+    const defaultHeight = isLarge ? h(5) : isBasic ? h(2.5) : isSmall ? h(1.25) : isTiny ? h(0.625): height
+    const defaultWidth = isLarge ? w(10) : isBasic ? w(5) : isSmall ? w(2.25) : isTiny ? w(1.25) : width
 
     return (
         <View
