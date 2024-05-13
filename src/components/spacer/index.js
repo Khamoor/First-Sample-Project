d4e0a2c5edc815fbc4e5de161c9b1f9ab3eb2d8c
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-13 18:54:18 +0500

    Adding 'isHorizontal' prop in 'Spacer' Component

--- src/components/spacer/index.js
+++ src/components/spacer/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { View } from 'react-native'
 import { height as h, width as w } from 'react-native-dimension'
 
-export default function Spacer({ isBasic, isSmall, isTiny, isLarge, height, width }) {
+export default function Spacer({ isBasic, isSmall, isTiny, isLarge, height, width, isHorizontal }) {
 
-    const defaultHeight = isLarge ? h(5) : isBasic ? h(2.5) : isSmall ? h(1.25) : isTiny ? h(0.625): height
+    const defaultHeight = isLarge ? h(5) : isBasic ? h(2.5) : isSmall ? h(1.25) : isTiny ? h(0.625) : height
     const defaultWidth = isLarge ? w(10) : isBasic ? w(5) : isSmall ? w(2.25) : isTiny ? w(1.25) : width
 
     return (
         <View
             style={{
-                height: defaultHeight,
-                width: defaultWidth,
+                height: !isHorizontal && defaultHeight,
+                width: isHorizontal || width && defaultWidth,
             }}
         />
     )
