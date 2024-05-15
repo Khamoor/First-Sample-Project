4a58221e71e663a8d7def7c7a9f71a3871142b94
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-15 20:33:01 +0500

    Added 'isColored', 'isBordered' and some margin & padding Horizontals props in 'Wrapper' Component. Also create const 'styles' function which define 'colored' and 'bordered' styles

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
-import { appStyles } from '../../services/utilities';
-import { width } from 'react-native-dimension';
+import { StyleSheet, View } from 'react-native';
+import { appStyles, colors } from '../../services/utilities';
+import { width, height } from 'react-native-dimension';
 
 export default function Wrapper({
     children,
     flex,
     isMain,
     center,
+    isColored,
+    isBordered,
+
     // Margin Horizontal
+    marginHorizontalLarge,
+    marginHorizontalMedium,
     marginHorizontalBase,
     marginHorizontalSmall,
     marginHorizontalTiny,
+
+    // Padding Horizontal
+    paddingHorizontalLarge,
+    paddingHorizontalMedium,
+    paddingHorizontalBase,
+    paddingHorizontalSmall,
+    paddingHorizontalTiny,
 
     flexDirectionRow,
     // Align Items
@@ -30,10 +42,23 @@ export default function Wrapper({
                 },
                 isMain && appStyles.mainContainer,
                 center && appStyles.center,
+
+                isColored && styles.colored,
+                isBordered && styles.bordered,
+
                 // Margin Horizontal
+                marginHorizontalLarge && { marginHorizontal: width(10) },
+                marginHorizontalMedium && { marginHorizontal: width(7.5) },
                 marginHorizontalBase && { marginHorizontal: width(5) },
                 marginHorizontalSmall && { marginHorizontal: width(2.5) },
                 marginHorizontalTiny && { marginHorizontal: width(1.25) },
+
+                // Padding Horizontal
+                paddingHorizontalLarge && { paddingHorizontal: width(10) },
+                paddingHorizontalMedium && { paddingHorizontal: width(7.5) },
+                paddingHorizontalBase && { paddingHorizontal: width(5) },
+                paddingHorizontalSmall && { paddingHorizontal: width(2.5) },
+                paddingHorizontalTiny && { paddingHorizontal: width(1.25) },
 
                 flexDirectionRow && { flexDirection: 'row' },
                 alignItemsCenter && { alignItems: 'center' },
@@ -47,3 +72,21 @@ export default function Wrapper({
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    colored: {
+        marginHorizontal: width(5),
+        borderRadius: 10,
+        backgroundColor: colors.appBackgrounColor2,
+        paddingVertical: height(2.5),
+        paddingHorizontal: width(5),
+    },
+    bordered: {
+        marginHorizontal: width(5),
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: colors.appBackgrounColor3,
+        paddingVertical: height(2.5),
+        paddingHorizontal: width(5),
+    }
+})
