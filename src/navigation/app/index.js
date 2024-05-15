6e1f7b4dd18bc834affee0e8762b42b9487c2fa8
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-15 20:53:50 +0500

    Creating 'AppNavigation' Component which creates  a bottom tab navigation interface for the main applicaiton, allowing users to easily switch between the 'Home' and 'Account' screens while also providing access to the menu screen

---
+++ src/navigation/app/index.js
@@ -0,0 +1,73 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { EditProfile, Home, Menu } from '../../screens/app';
+import { routes } from '../../services';
+import { colors, appStyles, appIcons } from '../../services/utilities';
+import { Icon, Image } from '@rneui/base';
+import { totalSize } from 'react-native-dimension';
+
+const Tab = createBottomTabNavigator();
+const AppStack = createNativeStackNavigator();
+
+// Defines a Bottom Tab Navigator with Two Tabs
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: { backgroundColor: colors.appBackgrounColor1, borderTopWidth: 0, ...appStyles.shadowDark },
+                tabBarLabelStyle: [appStyles.textSmall],
+            }}
+        >
+            {/* Home Tab */}
+            <Tab.Screen name={routes.home} component={Home}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: () => {
+                        return (
+                            <Icon
+                                name='home'
+                                type='feather'
+                                size={totalSize(2.5)}
+                            />
+                        )
+                    }
+                }}
+            />
+            {/* Account Tab */}
+            <Tab.Screen name={routes.editProfile} component={EditProfile}
+                options={{
+                    tabBarLabel: 'Account',
+                    tabBarIcon: () => {
+                        return (
+                            <Image
+                                source={appIcons.user}
+                                style={{ height: totalSize(2.5), width: totalSize(2.5) }}
+                                resizeMode='contain'
+                            />
+                            // <Icon
+                            //     name='home'
+                            //     type='feather'
+                            //     size={totalSize(2.5)}
+                            // />
+                        )
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+// Define Stack Navigator
+export default function AppNavigation() {
+    return (
+        <AppStack.Navigator
+            screenOptions={{ headerShown: false }}
+        >
+            {/* MyTabs is the Main Screen */}
+            <AppStack.Screen name={routes.bottomTab} component={MyTabs} />
+            {/* Menu is Snother Screen */}
+            <AppStack.Screen name={routes.menu} component={Menu} />
+        </AppStack.Navigator>
+    );
+}
