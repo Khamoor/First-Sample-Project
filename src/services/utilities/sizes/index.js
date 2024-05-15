77fdc32e8fb9580a3605eb79abf3b69ea332452a
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-15 18:40:17 +0500

    Changed sizes of fonts in 'fontSizes' const component

--- src/services/utilities/sizes/index.js
+++ src/services/utilities/sizes/index.js
@@ -2,17 +2,17 @@ import { totalSize } from 'react-native-dimension'
 
 export const fontSizes = {
     // Headings
-    h1: totalSize(4),
-    h2: totalSize(3.6),
-    h3: totalSize(3.2),
-    h4: totalSize(2.8),
-    h5: totalSize(2.4),
-    h6: totalSize(2),
+    h1: totalSize(4.4),
+    h2: totalSize(4),
+    h3: totalSize(3.6),
+    h4: totalSize(3.2),
+    h5: totalSize(2.8),
+    h6: totalSize(2.4),
     // Normal Text
-    large: totalSize(1.8),
-    medium: totalSize(1.6),
-    regular: totalSize(1.4),
-    small: totalSize(1.2),
-    tiny: totalSize(1),
-    xTiny: totalSize(0.8),
+    large: totalSize(2),
+    medium: totalSize(1.8),
+    regular: totalSize(1.6),
+    small: totalSize(1.4),
+    tiny: totalSize(1.2),
+    xTiny: totalSize(1),
 }
