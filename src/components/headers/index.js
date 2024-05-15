405b0749375d215659f367261571bfce49009864
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-15 20:29:53 +0500

    Creating 'Secondary' component which is used to styled background image component with a text overlay

--- src/components/headers/index.js
+++ src/components/headers/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Image, ImageBackground, StyleSheet, View } from 'react-native'
 import { height, width, totalSize } from 'react-native-dimension'
-import { colors } from '../../services/utilities'
+import { appImages, colors } from '../../services/utilities'
 import { Icon } from '@rneui/base'
 import Texts from '../texts'
+import Wrapper from '../wrapper'
 
 export function Primary({
     title,          // Represents title text to be displayed in the header
@@ -41,5 +42,26 @@ export function Primary({
                 </View>
             </View>
         </View>
+    )
+}
+
+export function Secondary({ title }) {
+    return (
+        <Wrapper style={{ height: height(20) }}>
+            <Wrapper style={StyleSheet.absoluteFill}>
+                <Image
+                    source={appImages.background_1}
+                    style={{ flex: 1, height: null, width: null, borderBottomRightRadius: 50, borderBottomLeftRadius: 50 }}
+                />
+            </Wrapper>
+            {/* <ImageBackground
+                source={appImages.background_1}
+                style={{ flex: 1, height: null, width: null, borderBottomRightRadius: 50, borderBottomLeftRadius: 50 }}
+            > */}
+            <Wrapper flex={1} center>
+                <Texts isH5 font1Medium isWhite>{title}</Texts>
+            </Wrapper>
+            {/* </ImageBackground> */}
+        </Wrapper>
     )
 }
