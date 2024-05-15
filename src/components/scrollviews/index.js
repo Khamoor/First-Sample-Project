cee43180826afa4bb5ef7f3ed501d15eae04cfe7
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-15 20:15:55 +0500

    Adding '{...props}' prop which allows us to pass any additional props from the parent component to the 'ScrollView' without having to explicitly define each prop in 'KeyboardAvoidingView' component.

--- src/components/scrollviews/index.js
+++ src/components/scrollviews/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
 
-export function KeyboardAvoiding({ children }) {
+export function KeyboardAvoiding({ children, ...props }) {
     return (
         <KeyboardAvoidingView
             style={{ flex: 1 }}
             behavior={Platform.OS === 'ios' ? 'padding' : null}
         >
-            <ScrollView>
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+                {...props}
+            >
                 {children}
             </ScrollView>
         </KeyboardAvoidingView>
