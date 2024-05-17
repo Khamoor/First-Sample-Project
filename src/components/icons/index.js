90e03b6181c76bba288c304653d02c1ba81fa403
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-17 21:53:52 +0500

    Added 'Button' component in 'Icons' component

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,5 +1,7 @@
-import { Image, Pressable } from "react-native"
+import { Image, Pressable } from 'react-native'
 import { height, width, totalSize } from 'react-native-dimension'
+import LinearGradient from 'react-native-linear-gradient';
+import { colors } from '../../services/utilities';
 
 export const Custom = ({ onPress, source, size, style }) => {
     const defaultSize = size || totalSize(2.5)
@@ -12,6 +14,26 @@ export const Custom = ({ onPress, source, size, style }) => {
                 style={[{ height: defaultSize, width: defaultSize }, style]}
                 resizeMode="contain"
             />
+        </Pressable>
+    )
+}
+
+export const Button = ({ onPress, buttonSize, gradientColors, customIcon, iconSize, buttonStyle }) => {
+    const defaultButtonSize = buttonSize || totalSize(5)
+    const defaultIconSize = iconSize || totalSize(3.5)
+    return (
+        <Pressable
+            onPress={onPress}
+        >
+            <LinearGradient
+                colors={gradientColors || colors.appGradient2}
+                style={[{ height: defaultButtonSize, width: defaultButtonSize, borderRadius: 100, alignItems: 'center', justifyContent: 'center' }, buttonStyle]}
+            >
+                <Custom
+                    source={customIcon}
+                    size={defaultIconSize}
+                />
+            </LinearGradient>
         </Pressable>
     )
 }
