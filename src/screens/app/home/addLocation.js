c9c507a2fa8701cdc3d7f462b5753b9807306dd2
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-05-18 06:53:23 +0500

    Creating 'AddLocation' component which is a modal dialog in a RN that allows users to add a location

---
+++ src/screens/app/home/addLocation.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Modal from "react-native-modal";
+import { Buttons, Icons, Spacer, Textinputs, Texts, Wrapper } from '../../../components';
+import { appIcons, appStyles, colors, fontSizes, fonts } from '../../../services/utilities';
+import { height, width } from 'react-native-dimension'
+
+export default function AddLocation({ isVisible, toggle }) {
+    return (
+        <Modal
+            isVisible={isVisible}
+            onBackdropPress={toggle}
+            style={{ margin: 0 }}
+        >
+            <Wrapper isColored>
+                <Wrapper
+                    style={{
+                        marginTop: -(width(17.5))
+                    }}
+                    alignItemsCenter>
+                    <Icons.Button
+                        customIcon={appIcons.location_2}
+                        buttonSize={width(25)}
+                        iconSize={width(12.5)}
+                    />
+                </Wrapper>
+                <Spacer isSmall />
+                <Texts isMedium font2Medium alignCenter>Add Location</Texts>
+                <Spacer isSmall />
+                <Textinputs.ColoredSecondary
+                    placeholder='Search here'
+                />
+                <Spacer isBasic />
+                <Wrapper alignItemsCenter>
+                    <Buttons.ColoredGradient
+                        title={'Submit'}
+                        gradientColors={colors.appGradient3}
+                        titleStyle={[appStyles.textRegular, appStyles.textFont2Regular, appStyles.textWhite]}
+                        gradientStyle={{ height: height(5), paddingHorizontal: width(10) }}
+                        onPress={toggle}
+                    />
+                </Wrapper>
+            </Wrapper>
+        </Modal>
+    )
+}
