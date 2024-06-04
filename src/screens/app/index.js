2cabe6b07a3a24246d6437cd64e981e86cb2b867
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-04 16:05:26 +0500

    Exporting 'SelectPriceAndPaymentMethod' screen

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -2,5 +2,6 @@ import Home from './home'
 import Menu from './menu'
 import EditProfile from './editProfile'
 import VehicleInfo from './vehicleInfo'
+import SelectPriceAndPaymentMethod from './selectPriceAndPaymentMethod'
 
-export { Home, Menu, EditProfile, VehicleInfo }
+export { Home, Menu, EditProfile, VehicleInfo, SelectPriceAndPaymentMethod }
