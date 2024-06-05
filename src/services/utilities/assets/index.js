50f4fbf57f1b6c664efc7096ffc20b3e7a4e0aef
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-05 16:27:13 +0500

    Exporting logos as assets for RN application

--- src/services/utilities/assets/index.js
+++ src/services/utilities/assets/index.js
@@ -1,6 +1,12 @@
 export const appImages = {
     background_1: require('../../../assets/images/background_1.png'),
     logo_1: require('../../../assets/images/logo_1.png'),
+    affirm_logo: require('../../../assets/images/affirm_logo.png'),
+    android_pay_logo: require('../../../assets/images/android_pay_logo.png'),
+    apple_pay_logo: require('../../../assets/images/apple_pay_logo.png'),
+    bitPay_logo: require('../../../assets/images/bitPay_logo.png'),
+    klarna_Logo: require('../../../assets/images/klarna_Logo.png'),
+    stripe_logo: require('../../../assets/images/stripe_logo.png'),
 }
 export const appIcons = {
     calendar: require('../../../assets/icons/calendar.png'),
