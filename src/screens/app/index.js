4f3ddfb1f48faf0ce95a78f7fa957f3f56fc2487
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-07 18:52:07 +0500

    Exporting 'ShareFeedback' component

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -3,5 +3,6 @@ import Menu from './menu'
 import EditProfile from './editProfile'
 import VehicleInfo from './vehicleInfo'
 import SelectPriceAndPaymentMethod from './selectPriceAndPaymentMethod'
+import ShareFeedback from './shareFeedback'
 
-export { Home, Menu, EditProfile, VehicleInfo, SelectPriceAndPaymentMethod }
+export { Home, Menu, EditProfile, VehicleInfo, SelectPriceAndPaymentMethod, ShareFeedback }
