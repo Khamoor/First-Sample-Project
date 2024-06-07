5fc2069052e8126552f8560c1e32e3eba987308e
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-07 18:57:25 +0500

    Adding 'shareFeedback' route in const 'routes' component

--- src/services/constants/index.js
+++ src/services/constants/index.js
@@ -14,4 +14,5 @@ export const routes = {
     editProfile: 'editProfile',
     vehicleInfo: 'vehicleInfo',
     selectPriceAndPaymentMethod: 'selectPriceAndPaymentMethod',
+    shareFeedback: 'shareFeedback'
 }
