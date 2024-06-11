c2e21af3631870f4b17e0f0a15b860871294d289
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-11 12:39:35 +0500

    Added 'navigate' & 'goBack' function which abstracts actions provided by navigation libraries like React Navigation. 'navigate' takes a route names as first argument and an optional 'params' object as it second argument. Similarly the'goBack' navigates back in the navigation stack.

--- src/navigation/rootNavigation/index.js
+++ src/navigation/rootNavigation/index.js
@@ -1,3 +1,11 @@
 import { createRef } from "react";
 
 export const navigationRef = createRef()
+
+export const navigate = (route, params) => {
+    navigationRef?.current.navigate(route, params)
+}
+
+export const goBack = () => {
+    navigationRef?.current.goBack()
+}
