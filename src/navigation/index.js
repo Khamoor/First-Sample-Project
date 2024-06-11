4072dc49ee906625ba96cd7893c54f9313470e12
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-11 09:47:22 +0500

    Added 'navigationRef' which allows navigation actions to be performed from outside the navigation components.

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,12 +3,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthNavigation from './auth';
 import AppNavigation from './app';
 import { routes } from '../services';
+import { navigationRef } from './rootNavigation';
 
 const NavigationStack = createNativeStackNavigator();
 
 export default function Navigation() {
     return (
-        <NavigationContainer>
+        <NavigationContainer
+            ref={navigationRef}>
             <NavigationStack.Navigator
                 screenOptions={{ headerShown: false }}  // Hidding Header of App
                 initialRouteName={routes.app}  // Default route to render on first load of the navigator
