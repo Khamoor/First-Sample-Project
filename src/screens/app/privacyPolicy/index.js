12fcaef5d0101affad747f2d64fa22a892c68923
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2024-06-11 12:41:38 +0500

    Created a component which presents the 'privacy policy' text in a readable format within a scrollable container and provides navigation functionality to go back to the previous screen.

---
+++ src/screens/app/privacyPolicy/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Common, Headers, Scrollviews, Spacer, Texts, Wrapper } from '../../../components'
+import { goBack } from '../../../navigation/rootNavigation'
+
+export default function Index() {
+    const text = 'Integer at faucibus urna. Nullam condimentum leo id elit sagittis auctor. Curabitur elementum nunc a leo imperdiet, nec elementum diam elementum. Etiam elementum euismod commodo. Proin eleifend eget quam ut efficitur. Mauris a accumsan mauris.\n\nPhasellus egestas et risus sit amet hendrerit. Nulla facilisi. Cras urna sem, vulputate sed condimentum a, posuere vel enim.\n\nInteger at faucibus urna. Nullam condimentum leo id elit sagittis auctor. Curabitur elementum nunc a leo imperdiet, nec elementum diam elementum. Etiam elementum euismod commodo.\n\nProin eleifend eget quam ut efficitur. Mauris a accumsan mauris. Phasellus egestas et risus sit amet hendrerit. Nulla facilisi. Cras urna sem, vulputate sed condimentum a, posuere vel enim.\n\nInteger at faucibus urna. Nullam condimentum leo id elit sagittis auctor. Curabitur elementum nunc a leo imperdiet, nec elementum diam elementum. Etiam elementum euismod commodo. Proin eleifend eget quam ut efficitur. Mauris a accumsan mauris.\n\nPhasellus egestas et risus sit amet hendrerit. Nulla facilisi. Cras urna sem, vulputate sed condimentum a, posuere vel enim.'
+    return (
+        <Wrapper isMain>
+            <Headers.Primary
+                title={'Privacy Policy'}
+                onPressBack={goBack}
+            />
+            <Common.BgImageWrapper>
+                <Scrollviews.KeyboardAvoiding>
+                    <Spacer isLarge />
+                    <Wrapper marginHorizontalBase>
+                        <Texts isMedium colorPrimary>{text}</Texts>
+                    </Wrapper>
+                    <Spacer isLarge />
+                </Scrollviews.KeyboardAvoiding>
+            </Common.BgImageWrapper>
+        </Wrapper>
+    )
+}
